d3c89cc4ab0de876007533e494e809319cfde5eb
Computer-error <valeroso.er@obmontessori.edu.ph>
2026-03-01 13:33:58 +0800

    Add files via upload

--- main1.py
+++ main1.py
@@ -3,18 +3,18 @@ from pyscript import document
 def reveal(event):
     un = (document.getElementById("me"))
     pw = (document.getElementById("pw"))
-    char_countun = sum(char.isdigit() for char in un)+sum(char.isalpha() for char in un)
-    digit_countpw = sum(char.isdigit() for char in pw)
-    alpha_countpw = sum(char.isalpha() for char in pw)
-    countpw = alpha_countpw + digit_countpw
-    result = ""
+    char_countun = len(un)
+    digit_countpw = round(sum(char.isdigit() for char in pw))
+    alpha_countpw = round(sum(char.isalpha() for char in pw))
+    countpw = len(pw)
+    result = "none"
 
     if countpw >=10 and char_countun >=7:
         if alpha_countpw >=1 and digit_countpw >=1:
             result = "✅ Your username and password are valid, account successfully created."
         else:
-            result = "❌ Your password must contain at least one letter and one digit."
+            result = "❌ Your password must contain at least one letter or one digit."
     else:
         result = "❌Please make sure your username is at least 7 characters long and your password is at least 10 characters long."
 
-    document.getElementById("ver").innerText = result
+    document.getElementById("veri").innerText = result
